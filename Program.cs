fae20ebf763efa42334250aa857e9094293f44b8
Ajaya Shrestha <ajaya92@gmail.com>
2021-11-30 19:48:24 -0600

    Variables setup

--- Program.cs
+++ Program.cs
@@ -1,2 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace PasswordChecker
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int minLength = 0;
+            string upperCase = "";
+            string lowerCase = "";
+            string digits = "";
+            string specialChars = "";
+            Console.WriteLine("Enter your password:");
+            string password = Console.ReadLine();
+
+            int score = 0; 
+            if(password.Length >= minLength)
+            {
+                score++;
+            }
+
+            if(Tools.Contains(password, upperCase))
+            {
+                score++;
+            }
+
+            if (Tools.Contains(password, lowerCase))
+            {
+                score++;
+            }
+            if (Tools.Contains(password, digits))
+            {
+                score++;
+            }
+
+            if (Tools.Contains(password, specialChars))
+            {
+                score++;
+            }
+
+           Console.WriteLine(score);
+        }
+    }
+}
